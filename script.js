f725c5f15a3067227d70d234115a922512bdd6f7
Bishonenlover <44757496+bishonenlover@users.noreply.github.com>
2019-04-08 18:44:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  function copy(that){
+function copy(that){
 var inp = document.createElement('input');
 var pop = document.querySelector('.all');
 document.body.appendChild(inp)
@@ -7,10 +7,12 @@ inp.select();
 document.execCommand('copy');
 inp.remove();
 pop.classList.add('opened');
+document.body.classList.add('fix');
 }
 
 var popClose = document.querySelector('.all');
 var close = document.querySelector('.close, .all');
 close.onclick = function() {
 popClose.classList.remove('opened');
+document.body.classList.remove('fix');
 }
